c3ce4149a074e5bd0bdd4ca9ba32f7d2200a44a6
Dyunika <fendersiucul2@gmail.com>
2022-01-24 11:09:59 +0700

    yaya

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    return (
+       <div className='Navbar'>
+           <div className='container'>
+               <div className='w-3/12'></div>
+                  <div className='logo text-center upperecaSE 2-XL'>
+                      Yani 
+                  </div>
+               <div className='w-6/12'></div>
+                  <div className='nav'></div>
+               <div className='w-3/12'></div>
+
+           </div>
+       </div>>
+    );
 }
 
 export default App;
