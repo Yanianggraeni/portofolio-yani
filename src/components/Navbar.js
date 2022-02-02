50df087a86c93107987892a8153b4a06bb21065c
Dyunika <fendersiucul2@gmail.com>
2022-02-02 07:59:46 +0700

    yaniii

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,27 +3,33 @@ import Logo from './Logo';
 import Nav from './Nav';
 function Navbar() {
     return (
-        <div className='navbar bg-violet-600 items-center'>
-        <div className='container flex items-center mx-auto py-12'>
+        <div className='navbar bg-violet-600 '>
+        <div className='container flex items-center mx-auto'>
             <div className='w-3/12'>
-                 <div className='Logo text-center text-white upperecase text-2xl font-semibold'>
-                     Yani Anggraeni
-                 </div>
+                 <div>
+                    <Logo/>
                  </div>
 
                  <div className='w-6/12'>
-                     <ul className='nav flex justify-center'>
-                         <li className='nav-item text-center text-white text-opacity-60 text-lg font-semibold'>Profile</li>
+                     <ul className='nav nav-items flex justify-center space-x-10 py-16'>
+                        <li>
+                            <a className='nav-item text-center text-white text-opacity-60 text-lg font-semibold'>Profile</a>
+                        </li>
+                        <li>
+                            <a className='nav-item text-center text-white text-opacity-60 text-lg font-semibold'>Skills</a>
+                        </li>
+                        <li>
+                            <a className='nav-item text-center text-white text-opacity-60 text-lg font-semibold'>Project</a>
+                        </li>
+                        <li>
+                            <a className='nav-item text-center text-white text-opacity-60 text-lg font-semibold'>contact</a>
+                        </li>
                      </ul>
-                 </div>
+                </div>
 
-                 <div className='w-3/12'>
-                     <div className='button text-center'>
-                         <a className='text-yellow-300 text-lg border rounded-full border-yellow-300 outline-yellow-300 font-semiboldpy-2 px-6'>Kontak</a>
-                     </div>
-                 </div>
-             </div>
-         </div>
+            </div>
+        </div>
+        </div>
     );
 }
 
